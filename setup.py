f558809c2c6c7565e88e1b509e3ed3adcb0638f2
Yu (Albert) Jiang <yajucb@berkeley.edu>
2022-12-01 12:21:26 -0800

    clean

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='bolides_bounding_box',
-    version='0.0.1',
+    version='0.0.2',
     author='yajucb',
     author_email='yajucb@berkely.edu',
     packages=find_packages(),
-    #   url='http://pypi.python.org/pypi/PackageName/',
+    url='http://pypi.python.org/pypi/bolides-bounding-box/',
     license='LICENSE.txt',
     description='A package to get bounding box images from GOES satellites.',
     long_description=open('README.md').read(),
