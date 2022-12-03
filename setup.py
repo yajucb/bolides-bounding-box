f660a667b92bcbd7142cfbdc66fe859493d671f3
Yu (Albert) Jiang <yajucb@berkeley.edu>
2022-12-03 09:11:01 -0800

    forgot to bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bolides_bounding_box',
-    version='0.0.2',
+    version='0.0.3',
     author='yajucb',
     author_email='yajucb@berkely.edu',
     packages=find_packages(),
